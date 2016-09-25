c525170d1a3ecc2490d5b67c39fe64637df1139d
Leo Szeto <lunerfox@gmail.com>
2016-09-25 12:59:22 -0700

    Initial Commit. Imported various assets for game.

---
+++ Assets/PlayerInputHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//The player input handler handles a single player's input and translates it to
+//a force vector for the Bug Controller.
+
+public class PlayerInputHandler : MonoBehaviour {
+
+    public enum InputMethod {touchJoystick};
+    public InputMethod inputMethod;
+
+    public string horizontalAxisName = "Horizontal"; // The name given to the horizontal axis for the cross platform input
+    public string verticalAxisName = "Vertical"; // The name given to the vertical axis for the cross platform input
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
