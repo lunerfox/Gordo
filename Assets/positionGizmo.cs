f29ed30f3499122a93543df6de77458bd44c8d27
Leo Szeto <lunerfox@gmail.com>
2016-09-28 10:18:09 -0700

    Added a position Gizmo so we can see the spawnpoints more easily.

---
+++ Assets/positionGizmo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class positionGizmo : MonoBehaviour {
+
+    public float radius = 0.25f;
+    public Color color = Color.red;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+
+}
